83994bcf41eb98856c287bcb2fbd54dabf16450e
Rushikesh <todkarrushihikesh3.rt@gmail.com>
2026-01-16 08:18:40 +0530

    This is my secomnd version

--- calculator.sh
+++ calculator.sh
@@ -1,1 +1,3 @@
-Hi This file is just an example of git --This is my first line after adding file to git 
+Hi This file is just an example of git --This is my first line after adding file to git
+
+Hi this is my second line to check git status of thr file
